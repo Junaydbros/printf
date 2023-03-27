0931450ca20d065415db97dfe803ec9184796f94
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2023-03-27 15:34:37 +0100

    the printf function FIX #7

--- _printf.c
+++ _printf.c
@@ -10,16 +10,38 @@
 
 int _printf(const char *format, ...)
 {
-	int i = 0, count = 0;
-	char *str;
+	int count;
 	va_list args;/*declare list*/
 
-	format == NULL ? return (-1) : i = i;
+	if (format == NULL)
+		return (-1);
 	va_start(args, format); /*start va list*/
+
+	count = handle_format_string(format, args);
+
+	va_end(args);/*end va_list*/
+
+	return (count);
+}
+
+/**
+* handle_format_specifier - handles format specifier & prints equivalent value
+*
+* @specifier: the format specifier character
+* @args: the va_list object containing the variadic arguments
+*
+* Return: number of charater printed
+*/
+int handle_format_string(const char *format, va_list args)
+{
+	int i = 0, count = 0;
+	char *str;
+
 	while (format && format[i])
 	{
 		if (format[i] == '%')
 		{
+
 			switch (format[i + 1]) /*return variadic arguments*/
 			{
 				case 'c':
@@ -29,25 +51,29 @@ int _printf(const char *format, ...)
 				case 's':
 					i += 2;
 					str = va_arg(args, char *);
-					str == NULL ? break : i = i;
+					if (str == NULL)
+						break;
 					count += _puts(str);
 					break;
 				case '%':
 					count += _putchar('%');
 					i += 2;
 					break;
+				case '\0':
+					return (-1);
 				default:
 					count += _putchar(format[i]);
 					i += 1;
 					break;
-			}
-		}
+				}
+		}	
 		else
 		{
-			format[i] != '\0' ? count += _putchar(format[i]) : i = i;
+			count += _putchar(format[i]);
 			i++;
 		}
+
 	}
-	va_end(args);/*end va_list*/
+
 	return (count);
 }
